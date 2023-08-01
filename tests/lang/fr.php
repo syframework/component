002002cf03fd553b7ff959fe29bac0c8a24ad135
Syone <syone7@gmail.com>
2023-08-01 22:50:54 +0200

    upgrade phpunit to v10

--- tests/lang/fr.php
+++ tests/lang/fr.php
@@ -7,4 +7,8 @@ return array(
 	'a pineapple' => 'un ananas',
 	'This is %s' => 'Ceci est %s',
 	'Number of %d max' => 'Nombre de %d max',
+	'My question? Ok' => 'Ma question ? Ok',
+	"It's me" => "C'est moi",
+	"Ok, good" => "Ok, bien",
+	"Full stop." => "Point final.",
 );
